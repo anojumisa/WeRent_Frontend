abaaa821ea7db308e26928c616653c18dbe24d20
Frisqia <frifani123@gmail.com>
2024-11-10 21:33:08 +0700

    install schdn-ui component to design

---
+++ we-rent/src/pages/product/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "./layout";
+
+export default function ProductPage() {
+  return (
+    <Layout>
+      <div>
+        <h1>Product Page Content</h1>
+        {/* Additional content for the Product Page can go here */}
+      </div>
+    </Layout>
+  );
+}
