d8369bc2810ad664453d9a6416efe529f9eb9e3b
leonard800 <leonardabimanyu@gmail.com>
2024-11-13 20:10:19 +0700

    test

---
+++ we-rent/src/pages/components/header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <main>
+            <div>
+                <div>
+                <img 
+                    src="/werent-logo.svg"
+                    width={500}
+                    height={500}
+                    alt="WeRent Logo"
+                />
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Product</li>
+                </ul>
+                </div>
+            </div>
+        </main>
+        
+    );
+}
+
+export default Header;
