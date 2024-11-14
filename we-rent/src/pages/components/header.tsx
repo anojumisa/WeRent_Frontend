1f58a6599d82c0858b3527f7a5894e3bb71dcd61
leonard800 <leonardabimanyu@gmail.com>
2024-11-14 09:18:54 +0700

    header in progress

--- we-rent/src/pages/components/header.tsx
+++ we-rent/src/pages/components/header.tsx
@@ -2,24 +2,27 @@ import React from "react";
 
 const Header = () => {
     return (
-        <main>
-            <div>
-                <div>
-                <img 
-                    src="/werent-logo.svg"
-                    width={500}
-                    height={500}
-                    alt="WeRent Logo"
-                />
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Product</li>
-                </ul>
+            <div className="w-full h-20 lg:h-28 border-b-[1px] border-gray-500 text-black lg:text-white bg-white lg:bg-transparent">
+                <div className="max-w-screen-2xl h-full mx-auto px-4 flex items-center justify-between">
+                    <img 
+                        src="/werent-logo.svg"
+                        width={500}
+                        height={500}
+                        alt="WeRent Logo"
+                    />
+                    <ul className="hidden lg:inline-flex items-center gap-8 uppercase text-sm font-semibold">
+                        <li className="navbarLi">Home</li>
+                        <li className="navbarLi">About</li>
+                        <li className="navbarLi">Product</li>
+                    </ul>
+
+                    <div className="hidden lg:inline-flex gap-8 items-center">
+                        <button className="w-48 h-14 bg-white text-black uppercase text-sm font-semibold rounded-md hover:bg-darkRed hover:text-white duration-300">
+                            Login
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </main>
-        
+            </div>        
     );
 }
 
