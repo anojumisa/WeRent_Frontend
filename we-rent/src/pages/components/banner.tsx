e016b8836001e06b9cb189667414105a4c57408f
leonard800 <leonardabimanyu@gmail.com>
2024-11-14 21:30:46 +0700

    banner and header revision

--- we-rent/src/pages/components/banner.tsx
+++ we-rent/src/pages/components/banner.tsx
@@ -5,11 +5,12 @@ import React from "react";
 const Banner = () => {
     return (
         <div
-            className="h-96 max-w-screen-2xl mx-auto flex flex-col justify-center items-center bg-[url('/werent-banner.jpg')] bg-cover bg-center"
+            className="h-96 max-w-screen-2xl mx-auto flex flex-col justify-center items-center bg-[url('/werent-banner.jpg')] bg-cover bg-center mt-20 lg:mt-28 text-white"
         >
-            <h1 className="text-2xl md:text-4xl uppercase font-bold text-white drop-shadow-md">
+            <h1 className="text-2xl md:text-4xl uppercase font-bold drop-shadow-md">
                 We Rent
             </h1>
+            <p className="mt-4 text-lg md:text-xl">Providing best user experience</p>
         </div>
     );
 };
--- we-rent/src/pages/components/header.tsx
+++ we-rent/src/pages/components/header.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 
 const Header: React.FC = () => {
     return (
-      <div className="w-full h-20 lg:h-28 border-b-[1px] border-gray-500 text-black lg:text-white bg-white lg:bg-transparent">
+        <div className="fixed top-0 w-full h-20 lg:h-28 z-50 text-white bg-transparent">
             <div className="max-w-screen-2xl h-full mx-auto px-4 flex items-center justify-between">
+                {/* Logo */}
                 <Image 
                     src="/werent-logo.svg"
                     width={150}
@@ -14,16 +15,19 @@ const Header: React.FC = () => {
                     alt="WeRent Logo"
                     priority
                 />
-                <ul className="hidden lg:inline-flex items-center gap-8 uppercase text-sm font-semibold">
+                
+                {/* Navbar Links */}
+                <ul className="hidden lg:flex items-center gap-8 uppercase text-sm font-semibold">
                     {["Home", "About", "Product"].map((item) => (
-                        <li key={item} className="navbarLi cursor-pointer hover:text-gray-300">
+                        <li key={item} className="cursor-pointer hover:text-gray-300">
                             {item}
                         </li>
                     ))}
                 </ul>
 
-                <div className="hidden lg:inline-flex gap-8 items-center">
-                    <button className="w-48 h-14 bg-white text-lime-950 uppercase text-sm font-semibold rounded-md hover:bg-darkRed hover:text-lime-900 duration-300">
+                {/* Login Button */}
+                <div className="hidden lg:flex gap-8 items-center">
+                    <button className="w-36 h-12 bg-white text-black uppercase text-sm font-semibold rounded-md hover:bg-gray-200 duration-300">
                         Login
                     </button>
                 </div>
