db95b58b6280ea0a6b700cb82a733d8c07ea12f1
leonard800 <leonardabimanyu@gmail.com>
2024-11-14 21:48:06 +0700

    featured items added and header fix

--- we-rent/src/pages/_document.tsx
+++ we-rent/src/pages/_document.tsx
@@ -1,13 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Header from "./components/header";
-import Banner from "./components/banner";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Header />
       <Head />
-      <Banner />
       <body className="antialiased">
         <Main />
         <NextScript />
--- we-rent/src/pages/components/header.tsx
+++ we-rent/src/pages/components/header.tsx
@@ -7,7 +7,6 @@ const Header: React.FC = () => {
     return (
         <div className="fixed top-0 w-full h-20 lg:h-28 z-50 text-white bg-transparent">
             <div className="max-w-screen-2xl h-full mx-auto px-4 flex items-center justify-between">
-                {/* Logo */}
                 <Image 
                     src="/werent-logo.svg"
                     width={150}
@@ -16,7 +15,6 @@ const Header: React.FC = () => {
                     priority
                 />
                 
-                {/* Navbar Links */}
                 <ul className="hidden lg:flex items-center gap-8 uppercase text-sm font-semibold">
                     {["Home", "About", "Product"].map((item) => (
                         <li key={item} className="cursor-pointer hover:text-gray-300">
@@ -25,7 +23,6 @@ const Header: React.FC = () => {
                     ))}
                 </ul>
 
-                {/* Login Button */}
                 <div className="hidden lg:flex gap-8 items-center">
                     <button className="w-36 h-12 bg-white text-black uppercase text-sm font-semibold rounded-md hover:bg-gray-200 duration-300">
                         Login
