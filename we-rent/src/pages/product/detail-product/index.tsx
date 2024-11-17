64c9259719613460f7ba4175d6590ede7005f215
Frisqia <frifani123@gmail.com>
2024-11-17 07:52:02 +0700

    replace image string from array , and rent fee to sticky, so that t can comply

--- we-rent/src/pages/product/detail-product/index.tsx
+++ we-rent/src/pages/product/detail-product/index.tsx
@@ -1,14 +1,6 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { CardContent } from "@/components/ui/card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
 import { useRouter } from "next/router";
 import StarRating from "./customrate";
 
@@ -18,17 +10,19 @@ interface Product {
   image_designer: string;
   designer_name: string;
   rating: number;
-  images: string[];
+  images: string;
   price: number;
-  passed: string;
+  fabric: string;
   fit: string;
   dimensions: Dimensions;
   review: Review[];
+  total_review: number;
+  average_rating: number;
 }
 
 interface Dimensions {
-  size: number;
-  waist: number;
+  size: string;
+  bust: number;
   length: number;
 }
 interface Review {
@@ -41,20 +35,9 @@ interface Review {
 export default function DetailProduct() {
   const router = useRouter();
   const { id } = router.query;
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [products, setProducts] = useState<Product[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
-  const modalRef = useRef<HTMLDivElement | null>(null);
-
-  const openModal = (index: number) => {
-    setCurrentImageIndex(index);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => setIsModalOpen(false);
 
   useEffect(() => {
     async function fetchAllProduct() {
@@ -73,23 +56,6 @@ export default function DetailProduct() {
     fetchAllProduct();
   }, [id]);
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        modalRef.current &&
-        !modalRef.current.contains(event.target as Node) &&
-        isModalOpen
-      ) {
-        closeModal();
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isModalOpen]);
-
   if (loading) {
     return <div>LOADING.....</div>;
   }
@@ -103,52 +69,50 @@ export default function DetailProduct() {
   }
 
   const product = products[0];
-  const reorderedImages = [
-    ...product.images.slice(currentImageIndex),
-    ...product.images.slice(0, currentImageIndex),
-  ];
-
-  
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 justify-center gap-10 p-10 max-w-7xl mx-auto">
-        <div className="mx-auto lg:max-w-lg">
-          <Carousel className="w-full max-w-lg">
-            <CarouselContent>
-              {product.images.map((img, index) => (
-                <CarouselItem key={index} onClick={() => openModal(index)}>
-                  <CardContent className="flex aspect-square items-center justify-center">
-                    <img
-                      src={img}
-                      alt={img}
-                      className="object-cover cursor-pointer w-[25rem] h-[35rem] p-3"
-                    />
-                  </CardContent>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious className="absolute left-0 top-1/2 transform -translate-y-1/2 ml-2" />
-            <CarouselNext className="absolute right-0 top-1/2 transform -translate-y-1/2 mr-2" />
-          </Carousel>
+      <div className="grid grid-cols-1 lg:grid-cols-2 justify-center gap-10 p-0 lg:p-10 max-w-7xl mx-auto text-black pb-20 ">
+        <div className="mx-auto lg:max-w-lg w-full">
+          <button
+            onClick={() => router.push("/product")}
+            className="lg:hidden px-4 py-2 text-lg font-bold items-center"
+          >
+            ❮
+          </button>
+          <img
+            src={product.images}
+            alt={product.title}
+            className="object-cover w-full h-[35rem] cursor-pointer"
+          />
         </div>
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center px-8 lg:p-0">
           <div className="flex flex-col font-sans">
-            <div className="divide-y">
-              <h1 className="text-sm md:text-lg lg:text-3xl mb-4 font-bold">
+            <div className="grid px-2">
+              <h1 className="text-2xl font-bold text-gray-800 mb-2 font-sans">
                 {product.title}
               </h1>
-              <div className="pt-5 flex justify-between items-center">
-                <div className="grid">
-                  <p className="text-xs md:text-sm lg:text-base">Rent Fee</p>
-                  <h2 className="text-sm md:text-base lg:text-lg">
-                    RP. {product.price.toLocaleString()} / Days
-                  </h2>
-                </div>
-                <button className="text-sm md:text-base lg:text-lg px-10 bg-yellow-500 rounded">
-                  ADD
-                </button>
+
+              <div className="flex items-center">
+                <StarRating rating={product.average_rating} />
+                <span className="ml-2 text-sm text-gray-400 ">
+                  {product.total_review} REVIEWS
+                </span>
               </div>
+            </div>
+            <div className="hidden lg:flex bottom-0 bg-white w-full z-10 py-4 flex justify-between items-center">
+              <div className="grid">
+                <p className="text-xs md:text-sm lg:text-base">Rent Fee</p>
+                <h2 className="text-sm md:text-base lg:text-lg">
+                  RP. {product.price.toLocaleString()}/ Days
+                </h2>
+              </div>
+              <button className="text-sm md:text-base lg:text-lg px-10 bg-yellow-500 rounded">
+                ADD
+              </button>
+            </div>
+            <div className="pt-4 border-b-2 border-black flex justify-between text-xs">
+              <p>{product.dimensions.size}</p> <p>VIEW SIZE GUIDE</p>
             </div>
             <div className="text-xs md:text-base lg:text-lg grid gap-4 mt-4">
               <div className="flex justify-between items-center">
@@ -156,12 +120,13 @@ export default function DetailProduct() {
                 <p>VIEW THE COLLECTION</p>
               </div>
               <div
-                className="bg-cover bg-center py-2 px-4 mb-4 w-full h-[5rem] p-3 font-Qwitcher"
+                className="bg-cover bg-center mb-4 w-full h-[5rem] font-Qwitcher text-2xl flex items-center"
                 style={{ backgroundImage: `url(${product.image_designer})` }}
               >
-                {product.designer_name}
+                <span className="pl-4">{product.designer_name}</span>
               </div>
-              <div className="border-t-2 border-black mt-4"></div>
+
+              <div className="border-t-2 border-black mt-0"></div>
               <table className="table-auto w-full">
                 <thead>
                   <tr>
@@ -172,8 +137,8 @@ export default function DetailProduct() {
                 </thead>
                 <tbody>
                   <tr>
-                    <td className="font-bold text-left w-1/2">PASSED</td>
-                    <td className="text-right w-1/2">{product.passed}</td>
+                    <td className="font-bold text-left w-1/2">FABRIC</td>
+                    <td className="text-right w-1/2">{product.fabric}</td>
                   </tr>
                   <tr>
                     <td className="font-bold py-2 text-left w-1/2">FIT</td>
@@ -190,7 +155,7 @@ export default function DetailProduct() {
                       SIZE
                     </th>
                     <th className="border-b border-gray-800 px-4 py-2 text-center">
-                      WAIST
+                      BUST
                     </th>
                     <th className="border-b border-gray-800 px-4 py-2 text-center">
                       LENGTH
@@ -203,7 +168,7 @@ export default function DetailProduct() {
                       {product.dimensions.size}
                     </td>
                     <td className="border-b border-gray-800 px-4 py-2 text-center">
-                      {product.dimensions.waist} cm
+                      {product.dimensions.bust} cm
                     </td>
                     <td className="border-b border-gray-800 px-4 py-2 text-center">
                       {product.dimensions.length} cm
@@ -214,64 +179,18 @@ export default function DetailProduct() {
             </div>
           </div>
         </div>
-
-        <div className="mt-8">
-          <h2 className="text-lg font-bold mb-4">Customer Reviews</h2>
-          {product.review && product.review.length > 0 ? (
-            <ul className="space-y-4">
-              {product.review.map((r, index) => (
-                <li
-                  key={index}
-                  className="border border-gray-300 p-4 rounded-md"
-                >
-                  <p className="text-sm font-semibold">{r.user_name}</p>
-                  <p className="text-sm text-yellow-500">
-                      <StarRating rating={parseFloat(r.rating)} />
-                    </p>
-                  
-                  <p className="text-sm text-gray-600">{r.created_at}</p>
-                  <p className="text-sm text-gray-600">{r.comment}</p>
-                  <p className="text-sm text-yellow-500">
-                    
-                  </p>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p className="text-gray-500">
-              No reviews available for this product.
-            </p>
-          )}
+        <div className="fixed lg:hidden bottom-0 left-0 right-0 bg-white border-t border-gray-300 z-10 pt-2 pb-4 flex justify-between items-center px-8 py-4 shadow-lg">
+          <div className="grid">
+            <p className="text-xs md:text-sm">Rent Fee</p>
+            <h2 className="text-base md:text-lg font-bold">
+              RP. {product.price.toLocaleString()}/ Days
+            </h2>
+          </div>
+          <button className="text-xs md:text-sm px-4 py-2 bg-yellow-500 rounded">
+            ADD
+          </button>
         </div>
       </div>
-
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-65 hidden lg:flex items-center justify-center z-50">
-          <div
-            ref={modalRef}
-            className="relative lg:max-w-lg p-3"
-            style={{ width: "90vw", maxWidth: "31.25rem" }}
-          >
-            <Carousel className="w-full justify-center">
-              <CarouselContent>
-                {reorderedImages.map((img, index) => (
-                  <CarouselItem key={index}>
-                    <CardContent className="flex aspect-square items-center justify-center">
-                      <img
-                        src={img}
-                        alt={img}
-                        className="object-cover cursor-pointer w-[25rem] h-[35rem] p-3"
-                      />
-                    </CardContent>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious className="absolute left-0 top-1/2 transform -translate-y-1/2 ml-2" />
-              <CarouselNext className="absolute right-0 top-1/2 transform -translate-y-1/2 mr-2" />
-            </Carousel>
-          </div>
-        </div>
-      )}
     </>
   );
 }
